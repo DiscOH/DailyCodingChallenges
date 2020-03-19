386d76bc50a3ad815ff69d5ba01caff4212a03f6
sleepysucralose <62371325+sleepysucralose@users.noreply.github.com>
2020-03-19 11:55:47 -0700

    Update add_your_methods_here.py
    i'm also pickle rick

--- DawnoftheFirstDay/tests/add_your_methods_here.py
+++ DawnoftheFirstDay/tests/add_your_methods_here.py
@@ -1,9 +1,8 @@
 from DawnoftheFirstDay.tests.aggregate_test import aggregate_test
-from DawnoftheFirstDay import ben
-from DawnoftheFirstDay import rawal
+from DawnoftheFirstDay import ben, rawal, pigglerig
 # from DawnoftheFirstDay import YOURNAME
 
 # ADD YOUR METHOD TO THIS LIST
-methods = [ben.jump, rawal.toEnd]
+methods = [ben.jump, rawal.toEnd, pigglerig.winnable]
 
 aggregate_test(methods)

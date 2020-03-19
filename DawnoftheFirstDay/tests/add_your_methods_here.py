7f87397e71177b7a8f469d4753e98b03c43aa65f
Ben Nelson <mr.superhappy@gmail.com>
2020-03-19 02:22:28 -0700

    added namespace to import just incase

--- DawnoftheFirstDay/tests/add_your_methods_here.py
+++ DawnoftheFirstDay/tests/add_your_methods_here.py
@@ -1,8 +1,8 @@
 from DawnoftheFirstDay.tests.aggregate_test import aggregate_test
-from DawnoftheFirstDay.ben import jump
+from DawnoftheFirstDay.ben import jump as ben_jump
 # from DawnoftheFirstDay.YOURNAME import YOURMETHOD
 
 # ADD YOUR METHOD TO THIS LIST
-methods = [jump]
+methods = [ben_jump]
 
 aggregate_test(methods)

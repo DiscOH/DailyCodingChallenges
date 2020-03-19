2e91c7af2761d0ec263e84b6a008d74211f58f71
Shree <rawalshreepal000@gmail.com>
2020-03-19 10:04:57 -0700

    Update add_your_methods_here.py

--- DawnoftheFirstDay/tests/add_your_methods_here.py
+++ DawnoftheFirstDay/tests/add_your_methods_here.py
@@ -1,8 +1,9 @@
 from DawnoftheFirstDay.tests.aggregate_test import aggregate_test
 from DawnoftheFirstDay import ben
+from DawnoftheFirstDay import rawal
 # from DawnoftheFirstDay import YOURNAME
 
 # ADD YOUR METHOD TO THIS LIST
-methods = [ben.jump]
+methods = [ben.jump, rawal.toEnd]
 
 aggregate_test(methods)

4cd7d9da7fe4fd98a45df9bfde40272f7030f595
Shree <rawalshreepal000@gmail.com>
2020-03-19 13:19:35 -0700

    Update rawal.py

--- DawnoftheFirstDay/rawal.py
+++ DawnoftheFirstDay/rawal.py
@@ -6,22 +6,23 @@ path = []
 def toEnd(arr, start, end):
   
   if start > end:
-    print("WINNER WINNER CHICKEN DINNER")
+    # print("WINNER WINNER CHICKEN DINNER")
     return path
 
   if arr[start] == 0:
-    print("You're Stuck LOSER")
+    # print("You're Stuck LOSER")
     return []
 
   if start == end:
-    print("LOSER")
+    # print("LOSER")
     return []
 
   path.append(start)
   return toEnd(arr, start + arr[start], end)
 
-
-# Added __main__ pattern to avoid accidental code execution on import
-if __name__ == '__main__':
-  arr = [int(i) for i in input().split()]
-  print("Index Traversed = ",(toEnd(arr, 0, len(arr)-1)))
+def getList(arr):
+  global path
+  path  = []
+  # arr = [int(i) for i in input().split()]
+  # print("Index Traversed = ",(toEnd(arr, 0, len(arr)-1)))
+  return toEnd(arr, 0, len(arr)-1)

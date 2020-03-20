9f6e2a5884af64f71d12ae294d1b10d5a12c5851
sleepysucralose <62371325+sleepysucralose@users.noreply.github.com>
2020-03-19 18:40:27 -0700

    reinitialized
    swapped around order that things update

--- DawnoftheFirstDay/pigglerig.py
+++ DawnoftheFirstDay/pigglerig.py
@@ -1,23 +1,24 @@
 def winnable(arr):
-    index = 0
+    index = 1
     step = arr[0]
-    path = []
-    for elem in arr:
-        if step == 0:
-            print('poohead')
-            return []
-        elif index == len(arr):
-            print('so close yet so far')
-            return []
-        else:
-            if elem > step:
-                path.append(index)
-                step += elem
-                index += 1
-                if step > len(arr):
-                    return path
-            else:
-                step -= 1
-                index += 1
-        return []
+    path = [0]
 
+    if len(arr) < 2:
+        return []
+    if step >= len(arr):
+        return [step]
+
+    for elem in arr[1:-1]:
+        if step == 0:
+            return []
+        if index == len(arr):
+            return []
+        if elem > step:
+            path.append(index)
+            index += 1
+            step = elem
+            if step > len(arr[index:-1]):
+                return path
+        else:
+            step -= 1
+            index += 1
